3ddc89f4dce59e67b1148dd5f7dbe0924ca3df27
ClementMiermont <clementmi_artworkandgraph@outlook.com>
2024-12-26 01:42:05 +0100

    better painter and better zoom

--- PaintApp/src/drawing_canvas.py
+++ PaintApp/src/drawing_canvas.py
@@ -2,17 +2,40 @@ from PyQt5.QtWidgets import QFrame
 from PyQt5.QtGui import QPainter, QPixmap, QColor
 from PyQt5.QtCore import Qt
 
-
-
 class DrawingCanvas(QFrame):
     def __init__(self, parent=None):
         super(DrawingCanvas, self).__init__(parent)
-        self.canvas = QPixmap(self.width(), self.height())
-        self.canvas.fill(Qt.white)
+        self.grid_size = 25
+        self.scale_factor = 25
+        self.min_scale_factor = 5
+        self.max_scale_factor = 100
+        self.canvas = QPixmap(self.grid_size, self.grid_size)
+        self.canvas.fill(Qt.blue)
         self.drawing = False
-        self.activate_tool = None  # Pencil or eraser
+        self.activate_tool = None
         self.pen_color = QColor(Qt.black)
+        self.parent().setStyleSheet("border: none;")
 
+    def wheelEvent(self, event):
+        angle = event.angleDelta().y()
+        if angle > 0: # Zoom in
+            self.scale_factor = min(self.scale_factor + 5, self.max_scale_factor)
+        elif angle < 0: # Zoom out
+            self.scale_factor = max(self.scale_factor - 5, self.min_scale_factor)
+        self.update()
+
+
+    def resizeEvent(self, event):
+        content_width = self.parent().width()
+        content_height = self.parent().height()
+
+        self.scale_factor = min(content_width // self.grid_size, content_height // self.grid_size)
+
+        self.canvas = QPixmap(self.grid_size, self.grid_size)
+        self.canvas.fill(Qt.white)
+        self.setGeometry(0, 0, content_width, content_height)
+        self.update()
+        super(DrawingCanvas, self).resizeEvent(event)
 
     def set_tool(self, tool):
         """Set the active tool"""
@@ -33,10 +56,10 @@ class DrawingCanvas(QFrame):
 
     def modify_pixel(self, event):
         """Modify a pixel on the canvas"""
-        x = event.pos().x()
-        y = event.pos().y()
+        x = (event.pos().x() - self.lineWidth()) // self.scale_factor
+        y = (event.pos().y() - self.lineWidth()) // self.scale_factor
 
-        if 0 <= x < self.canvas.width() and 0 <= y < self.canvas.height():
+        if 0 <= x < self.grid_size and 0 <= y < self.grid_size:
             painter = QPainter(self.canvas)
             if self.activate_tool == "pencil":
                 painter.setPen(self.pen_color)
@@ -47,18 +70,14 @@ class DrawingCanvas(QFrame):
             painter.end()
             self.update()
 
-    def resizeEvent(self, event):
-        """Handle resizing of the drawing area"""
-        new_canvas = QPixmap(self.width(), self.height())
-        new_canvas.fill(Qt.white)
-        painter = QPainter(new_canvas)
-        painter.drawPixmap(0, 0, self.canvas)  # Copy the old canvas content
-        painter.end()
-        self.canvas = new_canvas
-        super(DrawingCanvas, self).resizeEvent(event)
-
     def paintEvent(self, event):
         """Override paintEvent to draw the canvas"""
         painter = QPainter(self)
-        painter.drawPixmap(0, 0, self.canvas)
+        # painter.drawPixmap(0, 0, self.canvas)
+        for x in range(self.grid_size):
+            for y in range (self.grid_size):
+                color = self.canvas.toImage().pixelColor(x, y)
+                painter.fillRect(x * self.scale_factor, y * self.scale_factor, self.scale_factor, self.scale_factor, color)
         painter.end()
+
+   
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QPainter, QPixmap, QColor
+from PyQt5.QtGui import QPainter, QPixmap, QColor # type: ignore
 from PyQt5.QtCore import Qt, QPoint
 from PaintApp.py.paint import Ui_MainWindow
 from PaintApp.src.drawing_canvas import DrawingCanvas
@@ -14,13 +14,23 @@ class MainApp(QMainWindow):
         self.ui.setupUi(self)
 
         self.drawing_canvas = DrawingCanvas(self.ui.DrawingFrame)
-        self.drawing_canvas.setGeometry(0, 0, self.ui.DrawingFrame.width(), self.ui.DrawingFrame.height())
-        
+
+        self.adjust_drawing_canvas()
+
         #Connect the buttons
         self.ui.Pencil.setCheckable(True)
         self.ui.Pencil.clicked.connect(self.activate_pencil)
         self.ui.Eraser.setCheckable(True)
         self.ui.Eraser.clicked.connect(self.activate_eraser)
+
+    def adjust_drawing_canvas(self):
+        """Adjust the drawing canvas"""
+        self.drawing_canvas.setGeometry(0, 0, self.ui.DrawingFrame.width(), self.ui.DrawingFrame.height())
+        self.drawing_canvas.update()
+
+    def resizeEvent(self, event):
+        self.adjust_drawing_canvas()
+        super(MainApp, self).resizeEvent(event)    
 
     def activate_pencil(self):
         """Activate the pencil"""
@@ -37,6 +47,7 @@ class MainApp(QMainWindow):
             self.ui.Pencil.setChecked(False)
         else:
             self.drawing_canvas.set_tool(None)             
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
